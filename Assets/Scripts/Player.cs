9a6f5cd9522c5e6c7a0542e19925c384b4f3a0d8
JJCMose <jmoseley421@yahoo.com>
2016-03-06 13:16:19 -0500

    Initial Commit
    Unity 2d in 3d

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private bool jumpStop = true;
+    
+    
+    void Start()
+    {
+        speed = 10;
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+        
+        //Jump Sequence
+        if (Input.GetKeyDown("space") && jumpStop)
+        {
+            jumpStop = false;
+            for (int i = 0; i < 30; i++)
+            {
+                rb.AddForce(0, i, 0);
+            }
+
+          // if (rb.velocity.y == 0)
+            {
+                jumpStop = true;
+            }
+            
+        }
+
+
+        //Death
+        if (transform.position.y < -5)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        rb.AddForce (movement * speed);
+        //print(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //Collission Trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("WinCube"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex +1);
+        }
+    }
+
+
+
+}
